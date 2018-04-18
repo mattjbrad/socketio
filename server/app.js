64cd2696e6875ea47221c7c0806c1f401c7658b3
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-04-18 16:03:03 +0100

    welcome messages and joining messages

--- server/app.js
+++ server/app.js
@@ -15,6 +15,17 @@ app.use(express.static('public'));
 io.on('connection', (socket)=>{
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from:'Admin',
+        text:'Welcome',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from:'Admin',
+        text:'New User Joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) =>{
         io.emit('newMessage', {
             from:message.from,
@@ -22,6 +33,11 @@ io.on('connection', (socket)=>{
             createdAt: new Date().getTime()
         });
         console.log('Created Message - ', message);
+        // socket.broadcast.emit('newMessage', {
+        //     from:message.from,
+        //     text:message. text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', ()=>{
