9d6e31805c6cb9584d7312c06900c13d83f1cd95
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-04-20 11:02:22 +0100

    chat with users names included

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -19,7 +19,15 @@ function scrollToBottom(){
 }
 
 socket.on('connect', function () {
-    console.log('connected to server')
+    var params = jQuery.deparam(window.location.search);
+    socket.emit('join', params, function(err){
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No error');
+        }
+    });
 });
 
 socket.on('disconnect', function (){
@@ -53,6 +61,16 @@ socket.on('newLocationMessage', function(message){
 
     $('#thread').append(html);
     scrollToBottom();
+
+});
+
+socket.on('updateUserList', function(users) {
+    var ol = $('<ol></ol>');
+    users.forEach(function (user){
+        ol.append($('<li></li>').text(user));
+    });
+
+    $('#users').html(ol);
 
 });
 
--- server/app.js
+++ server/app.js
@@ -9,17 +9,34 @@ var io = socketIO(server);
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '..', '/public');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
+var users = new Users();
+
 const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
 io.on('connection', (socket)=>{
-    console.log('new user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    socket.on('join', (params, callback) => {
+        if(!isRealString(params.name) || !isRealString(params.name)) {
+            return callback('Name and room name are required');
+        }
+        
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined the room`));
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        callback();
+    });
 
     socket.on('createMessage', (message, callback) =>{
+        socket.
         io.emit('newMessage', generateMessage(message.from, message.text));
         //confirm the message was received
         callback();
@@ -29,7 +46,12 @@ io.on('connection', (socket)=>{
         io.emit('newLocationMessage', generateLocationMessage('Location', coords.lat, coords.long));
     });
     socket.on('disconnect', ()=>{
-        //do something
+        var user = users.removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left...`));
+        }
     })
 });
 
